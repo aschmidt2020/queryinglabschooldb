76557d68646e5acb5680a2a77554a8d81bb37bc3
Audrey Schmidt <audsch@outlook.com>
2021-12-13 11:16:29 -0600

    complete problem 4 list all students in programming ordered by grade

--- querying_lab/school_db/views.py
+++ querying_lab/school_db/views.py
@@ -57,9 +57,12 @@ def problem_three(request):
 def problem_four(request):
     # Find all students who are taking the Programming class. 
     # Order by their grade. 
-
+    programming_students = StudentCourse.objects.filter(course_id=4).order_by('grade')
+    
+    data_visualization = [item for item in programming_students]
+    
     context = {
-        'student_courses': None
+        'programming_students': programming_students
     }
     return render(request, 'school/four.html', context)
 
